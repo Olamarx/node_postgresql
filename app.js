d586fd72be8196c13d08d041aa4483704eab6131
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-19 19:35:47 +0100

    default .status(404).json({     status: "error",     message: "Page not found",   });

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./routes/authRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
     message: "Welcome to the API!",
+  });
+});
+
+app.use("/api/v1/auth", router);
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found",
   });
 });
 
